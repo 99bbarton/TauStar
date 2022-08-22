ed1c6344b2b13ea409d23035b47e39c625f6fad5
99bbarton <99bbarton@gmail.com>
2022-08-22 15:11:51 -0700

    Extrapolated SF making scripts

---
+++ Analysis/PhoID_FittedSFS/redoHighPtBinFits.py
@@ -0,0 +1,20 @@
+#Script to redo fitting of the high pt bins of completed TNP measurements
+#
+
+import os
+
+#List of bins to redo the fits of
+binList = ["30", "31", "32", "33", "34"]
+#List of fits to redo for each bin
+fitList = ["--doFit", "--doFit --altSig", "--doFit altBkg"]
+#List of config files
+configs = ["myPhoConfig2015_noVeto.py", "myPhoConfig2016_noVeto.py", "myPhoConfig2017_noVeto.py", "myPhoConfig2018_noVeto.py"]
+
+commandBase = "python tnpEGM_fitter.py etc/config/" 
+flag = "--flag passingMVA94XV2wp90"
+binBase = "--iBin "
+
+for config in configs:
+    for fit in fitList:
+        for bin in binList:
+            os.system(commandBase + config + flag + fit + binBase + bin)
