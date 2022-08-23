c5c1350a65007062f71c97aa018d43b3965a9bcc
99bbarton <99bbarton@gmail.com>
2022-08-23 11:54:52 -0700

    Script to redo highpt bin fits

--- Analysis/PhoID_FittedSFS/redoHighPtBinFits.py
+++ Analysis/PhoID_FittedSFS/redoHighPtBinFits.py
@@ -1,4 +1,4 @@
-#Script to redo fitting of the high pt bins of completed TNP measurements
+ #Script to redo fitting of the high pt bins of completed TNP measurements
 #
 
 import os
@@ -6,15 +6,20 @@ import os
 #List of bins to redo the fits of
 binList = ["30", "31", "32", "33", "34"]
 #List of fits to redo for each bin
-fitList = ["--doFit", "--doFit --altSig", "--doFit altBkg"]
+fitList = ["--doFit", "--doFit --altSig", "--doFit --altBkg"]
 #List of config files
 configs = ["myPhoConfig2015_noVeto.py", "myPhoConfig2016_noVeto.py", "myPhoConfig2017_noVeto.py", "myPhoConfig2018_noVeto.py"]
+#configs = ["myPhoConfig2015_noVeto.py"]
 
 commandBase = "python tnpEGM_fitter.py etc/config/" 
-flag = "--flag passingMVA94XV2wp90"
-binBase = "--iBin "
+flag = " --flag passingMVA94XV2wp90 "
+binBase = " --iBin "
 
 for config in configs:
     for fit in fitList:
-        for bin in binList:
-            os.system(commandBase + config + flag + fit + binBase + bin)
+        for binN in binList:
+            command = commandBase + config + flag + fit + binBase + binN
+            print("command = " + command)
+            os.system(command)
+
+    os.system(commandBase + config + flag + "--sumUp")
