ae2a31f2542a7716f8e30b40519b4d9009b06b50
Ben Barton <99bbarton@gmail.com>
2023-04-04 16:28:14 +0200

    Corrected tauh matching and addded custom reco eff binning per obj

--- Analysis/RecoEffProducer.py
+++ Analysis/RecoEffProducer.py
@@ -30,19 +30,31 @@ class RecoEffProducer(Module):
         photons = Collection(event, "Photon")
         taus = Collection(event, "Tau")
 
-        genPartRecoStatus = [-1] * event["nGenPart"]
+        genPartRecoStatus = [0] * event["nGenPart"]
         genVisTauRecoStatus = [0] * event["nGenVisTau"]
 
         #Loop through genParticles and denote which ones are valid to be matched to for the particles we care about by writing their negative pdgIDs, else 0
         pdgIDs = [11, -11, 13, -13, 22] #el, mu, pho
         for i, genPart in enumerate(genParts):
             if (genPart.pdgId in pdgIDs) and (genPart.statusFlags & 8192) > 0 and (genPart.status == 1): #8192 = isLastCopy, status=1 is stable
+                if (abs(genPart.pdgId) % 11 == 0) and genPart.pt <= 5.0: #Reco els and phos are only saved for pt>5 
+                    continue
+                #https://cms-nanoaod-integration.web.cern.ch/integration/cms-swCMSSW_10_6_X/mc106Xul17v2_doc.html#:~:text=Unclustered%20Energy%20Up-,Muon,-slimmedMuons%20after%20basic
+                if abs(genPart.pdgId) == 13 and genPart.pt <= 18.0: #Reco mu selection at link above. Note pt>3 are saved if also passing an ID. Ignore those here to only study reco eff
+                    continue
+                
                 if genPart.pdgId < 0:
                     genPartRecoStatus[i] = genPart.pdgId #Valid to be matched to later
                 else:
                     genPartRecoStatus[i] = -1*genPart.pdgId
             else:
                 genPartRecoStatus[i] = -1 #Not relevant for us
+
+                
+        for j, genVisTau in enumerate(genVisTaus):
+            if genVisTau.pt <= 18.0:
+                genVisTauRecoStatus[j] = -1 #Reco taus are pt>18
+                
 
         # Iterate through the relevant reco particles, setting the corresponding genParticles reco status to the reco particles pdgID
         for el in electrons:
@@ -54,12 +66,15 @@ class RecoEffProducer(Module):
         for pho in photons:
             if genPartRecoStatus[pho.genPartIdx] == -22:
                 genPartRecoStatus[pho.genPartIdx] = 22
+
         for tau in taus:
-            if tau.genPartFlav == 5: #If a hadronic tau decay
+            if tau.genPartFlav == 5: #If a hadronic tau decay, points to GenVisTau collection rather than GenPart
                 if tau.genPartIdx >= event["nGenVisTau"]:
                     print("WARNING tau.genPartIdx > nGenVisTau:  genParts[tau.genPartIdx].pdgID= " + str(genParts[tau.genPartIdx].pdgId))
+                elif deltaR(tau, genVisTaus[tau.genPartIdx]) < 0.15:
+                    genVisTauRecoStatus[tau.genPartIdx] = 15
                 else:
-                    genVisTauRecoStatus[tau.genPartIdx] == 15
+                    print("WARNING tau DR match failed: tau.eta, tau.phi : genVisTau.eta, genVisTau.phi = "+str(tau.eta)+", "+str(tau.phi)+" : "+str(genVisTaus[tau.genPartIdx].eta)+", "+str(genVisTaus[tau.genPartIdx].phi))
 
 
         self.out.fillBranch("Eff_recoPartsStatus", genPartRecoStatus)
--- Reinterpretation/Efficiencies/calcObjEffs.C
+++ Reinterpretation/Efficiencies/calcObjEffs.C
@@ -7,19 +7,21 @@
 #include "TCanvas.h"
 #include "TTree.h"
 #include "TCut.h"
+#include "TChain.h"
+#include "TStyle.h"
+#include <iostream>
 
-
-const int N_FILES = 3;
+const int N_FILES = 17; //17 if all bkgd MC
 
 
 
 //Assign files to a TChain for easy access
 void getTreesMCasChain(TChain& chain, TString year)
 {
-  //TString mcNames[N_FILES] = {"DYJetsToLL_M10to50", "DYJetsToLL_M50", "ST_s_channel", "ST_tW_antitop", "ST_tW_top", "ST_t_channel_antitop", "ST_t_channel_top", "TTGamma_Dilept", "TTGamma_SingleLept", "TTTo2L2Nu", "TTToSemiLeptonic", "WGToLNuG", "WJetsToLNu", "WW", "WZ", "ZGToLLG", "ZZ" };
-  //TString baseDir = "root://cmsxrootd.fnal.gov//store/user/fjensen/excitedTau_22022023/";
-  TString mcNames[N_FILES] = {"recoEffs_sigMC_500", "recoEffs_sigMC_2500", "recoEffs_sigMC_5000"};
-    TString baseDir = "../Data/";
+    TString mcNames[N_FILES] = {"DYJetsToLL_M10to50", "DYJetsToLL_M50", "ST_s_channel", "ST_tW_antitop", "ST_tW_top", "ST_t_channel_antitop", "ST_t_channel_top", "TTGamma_Dilept", "TTGamma_SingleLept", "TTTo2L2Nu", "TTToSemiLeptonic", "WGToLNuG", "WJetsToLNu", "WW", "WZ", "ZGToLLG", "ZZ" };
+    TString baseDir = "root://cmsxrootd.fnal.gov//store/user/fjensen/excitedTau_23032023/";
+    //TString mcNames[N_FILES] = {"recoEffs_sigMC_2500"};
+    //TString baseDir = "../Data/";
     char filepath[100];
 
     for (int fN = 0; fN < N_FILES; fN++)
@@ -35,7 +37,7 @@ TString buildEffsStr(bool incReco=true, bool incID=true, bool incTrig=true)
 {
     if (!incReco && !incID && !incTrig)
     {
-        cout << "\nReco, ID, and trig efficiencies are all excluded. Exiting..." << endl;
+        std::cout << "\nReco, ID, and trig efficiencies are all excluded. Exiting..." << endl;
         exit(1);
     }
 
@@ -64,23 +66,34 @@ TString buildEffsStr(bool incReco=true, bool incID=true, bool incTrig=true)
 
 //Include reco efficiencies in the histograms passed to the function
 //If the passed histograms are empty, adds the reco efficienicies. Otherwise, multiplies the existing values with the ones calculated here
-void incRecoEffs(TH2F* h_elEff, TH2F* h_muEff, TH2F* h_tauEff, TH2F* h_phoEff, TString year, TString effsStr, int nEtaBins, double etaBins[])
+void incRecoEffs(TH2F* h_elEff, TH2F* h_muEff, TH2F* h_tauEff, TH2F* h_phoEff, TString year, TString effsStr)
 {
     /*Whether each relevant gen particle is matched can be found in the branch "Eff_recoPartsStatus"
     If the status is a positive definite value, that gen particle matched to a reco'd particle with abs(pdgID) = that value
     If the value is instead negative, that gen particle did NOT match to a reco'd particle with abs(pdgID) = abs(that value)
     Any other value means the gen particle was not of the type/status we care about */
 
-    cout << "\nCalculating object reco efficiencies for " << year << "..." << endl;
+    //Define custom binning
+    double etaBins_EG[6] = {0, 0.8, 1.4442, 1.566, 2.0, 2.5};
+    TString etaBinLabels_EG[5] = {"[0, 0.8)", "[0.8, 1.4442)", "[1.4442, 1.566)", "[1.566, 2.0)", "[2.0, 2.5)"};
+    double etaBins_mu[5] = {0, 0.9, 1.2, 2.1, 2.4};
+    TString etaBinLabels_mu[4] = {"[0, 0.9)", "[0.9, 1.2)", "[1.2, 2.1)", "[2.1, 2.4)"};
+    double etaBins_tau[4] = {0, 1.46, 1.558, 2.3};
+    TString etaBinLabels_tau[3] = {"[0, 1.46)", "[1.46, 1.558)", "[1.558, 2.3)"};
+    double ptBins_mu[6] = {18, 50, 100, 200, 500, 1500};
+    double ptBins_EG[7] = {5, 50, 100, 200, 500, 1000, 4000};
+    double ptBins_tau[6] = {18, 50, 100, 200, 500, 1000};
 
-    TH2F* h_elEff_reco = new TH2F("h_elEff_rec_" + year, "Electron " + effsStr + " Efficiency;El eta;El pt", nEtaBins, etaBins, 20, 0, 2000);
-    TH2F* h_phoEff_reco = new TH2F("h_phoEff_reco_" + year, "Photon " + effsStr + " Efficiency;Pho eta;Pho pt", nEtaBins, etaBins, 20, 0, 2000);
-    TH2F* h_muEff_reco = new TH2F("h_muEff_reco_" + year, "Muon " + effsStr + " Efficiency;Mu eta;Mu pt", nEtaBins, etaBins, 20, 0, 2000);
-    //TH2F* h_tauEff_reco = TH2F("h_tauEff_" + year, "Tau_h " + effsStr + " Efficiency;Tau eta;Tau pt", nEtaBins, etaBins, 20, 0, 2000);
-    TH2F* h_elEff_den = new TH2F("h_elEff_den_" + year, "Electron " + effsStr + " Efficiency;El eta;El pt", nEtaBins, etaBins, 20, 0, 2000);
-    TH2F* h_phoEff_den = new TH2F("h_phoEff_den_" + year, "Photon " + effsStr + " Efficiency;Pho eta;Pho pt", nEtaBins, etaBins, 20, 0, 2000);
-    TH2F* h_muEff_den = new TH2F("h_muEff_den_" + year, "Muon " + effsStr + " Efficiency;Mu eta;Mu pt", nEtaBins, etaBins, 20, 0, 2000);
-    //TH2F* h_tauEff_den = TH2F("h_tauEff_den_" + year, "Tau_h " + effsStr + " Efficiency;Tau eta;Tau pt", nEtaBins, etaBins, 20, 0, 2000);
+    std::cout << "\nCalculating object reco efficiencies for " << year << "..." << endl;
+
+    TH2F* h_elEff_reco = new TH2F("h_elEff_reco_" + year, "Electron " + effsStr + " Efficiency;El eta;El pt", 5, etaBins_EG, 6, ptBins_EG);
+    TH2F* h_phoEff_reco = new TH2F("h_phoEff_reco_" + year, "Photon " + effsStr + " Efficiency;Pho eta;Pho pt", 5, etaBins_EG, 6, ptBins_EG);
+    TH2F* h_muEff_reco = new TH2F("h_muEff_reco_" + year, "Muon " + effsStr + " Efficiency;Mu eta;Mu pt", 4, etaBins_mu, 5, ptBins_mu);
+    TH2F* h_tauEff_reco = new TH2F("h_tauEff_reco_" + year, "Tau_h " + effsStr + " Efficiency;Tau eta;Tau pt", 3, etaBins_tau, 5, ptBins_tau);
+    TH2F* h_elEff_den = new TH2F("h_elEff_den_" + year, "Electron " + effsStr + " Efficiency;El eta;El pt", 5, etaBins_EG, 6, ptBins_EG);
+    TH2F* h_phoEff_den = new TH2F("h_phoEff_den_" + year, "Photon " + effsStr + " Efficiency;Pho eta;Pho pt", 5, etaBins_EG, 6, ptBins_EG);
+    TH2F* h_muEff_den = new TH2F("h_muEff_den_" + year, "Muon " + effsStr + " Efficiency;Mu eta;Mu pt", 4, etaBins_mu, 5, ptBins_mu);
+    TH2F* h_tauEff_den = new TH2F("h_tauEff_den_" + year, "Tau_h " + effsStr + " Efficiency;Tau eta;Tau pt", 3, etaBins_tau, 5, ptBins_tau);
 
     TChain chain("Events", "MC Background Files");
     getTreesMCasChain(chain, year);
@@ -89,18 +102,31 @@ void incRecoEffs(TH2F* h_elEff, TH2F* h_muEff, TH2F* h_tauEff, TH2F* h_phoEff, T
     int genPartRecoStatus[500]; //Arrays must be equal or longer in length than arrays in tree branches
     float genPartEtas[500];
     float genPartPts[500];
+    int nGenVisTau;
+    int genVisTauRecoStatus[500];
+    float genVisTauEtas[500];
+    float genVisTauPts[500];
 
     //Only read the branches of the trees we care about for efficiency
     chain.SetBranchStatus("*", false);
+
     chain.SetBranchStatus("nGenPart", true);
     chain.SetBranchStatus("Eff_recoPartsStatus", true);
     chain.SetBranchStatus("GenPart_eta", true);
     chain.SetBranchStatus("GenPart_pt", true);
+    chain.SetBranchStatus("nGenVisTau", true);
+    chain.SetBranchStatus("Eff_recoVisTausStatus", true);
+    chain.SetBranchStatus("GenVisTau_eta", true);
+    chain.SetBranchStatus("GenVisTau_pt", true);
 
     chain.SetBranchAddress("nGenPart", &nGenParts);
     chain.SetBranchAddress("Eff_recoPartsStatus", &genPartRecoStatus);
     chain.SetBranchAddress("GenPart_eta", &genPartEtas);
     chain.SetBranchAddress("GenPart_pt", &genPartPts);
+    chain.SetBranchAddress("nGenVisTau", &nGenVisTau);
+    chain.SetBranchAddress("Eff_recoVisTausStatus", &genVisTauRecoStatus);
+    chain.SetBranchAddress("GenVisTau_eta", &genVisTauEtas);
+    chain.SetBranchAddress("GenVisTau_pt", &genVisTauPts);
 
     //Fill the appropriate numerator and denominator histograms with eta/pt of matched and non-matched relevant gen particles
     for (int eN = 1; eN < chain.GetEntries(); eN++)
@@ -108,42 +134,63 @@ void incRecoEffs(TH2F* h_elEff, TH2F* h_muEff, TH2F* h_tauEff, TH2F* h_phoEff, T
         chain.GetEntry(eN);
 
         for (int gpN = 0; gpN < nGenParts; gpN++)
-            {   
-                //Particles we don't care about - continue to avoid unecessary comparisons below
-	        if (abs(genPartRecoStatus[gpN]) < 11)
+        {   
+            //Particles we don't care about - continue to avoid unecessary comparisons below
+            if (abs(genPartRecoStatus[gpN]) < 11)
+                continue;
+            //Electrons
+            if (genPartRecoStatus[gpN] == 11)
+            {
+                h_elEff_reco->Fill(abs(genPartEtas[gpN]), genPartPts[gpN]);
+                h_elEff_den->Fill(abs(genPartEtas[gpN]), genPartPts[gpN]);
+            }  
+            else if (genPartRecoStatus[gpN] == -11)
+            {
+                h_elEff_den->Fill(abs(genPartEtas[gpN]), genPartPts[gpN]);
+            }  
+            //Muons
+            else if (genPartRecoStatus[gpN] == 13)
+            {
+                h_muEff_reco->Fill(abs(genPartEtas[gpN]), genPartPts[gpN]);
+                h_muEff_den->Fill(abs(genPartEtas[gpN]), genPartPts[gpN]);
+            }  
+            else if (genPartRecoStatus[gpN] == -13)
+            {
+                h_muEff_den->Fill(abs(genPartEtas[gpN]), genPartPts[gpN]);
+            }  
+            //Photons
+            else if (genPartRecoStatus[gpN] == 22)
+            {
+                h_phoEff_reco->Fill(abs(genPartEtas[gpN]), genPartPts[gpN]);
+                h_phoEff_den->Fill(abs(genPartEtas[gpN]), genPartPts[gpN]);
+            }  
+            else if (genPartRecoStatus[gpN] == -22)
+            {
+                h_phoEff_den->Fill(abs(genPartEtas[gpN]), genPartPts[gpN]);
+            }  
+        }
+
+        //Taus match separately to genVisTaus
+        if (nGenVisTau >= 1)
+        {
+            for (int gvtN = 0; gvtN < nGenVisTau; gvtN++)
+            {
+                if (genVisTauRecoStatus[gvtN] == -1) //Not a valid genVisTau for consideration
                     continue;
-                //Electrons
-                if (genPartRecoStatus[gpN] == 11)
+                else if (genVisTauRecoStatus[gvtN] == 15)
                 {
-                    h_elEff_reco->Fill(abs(genPartEtas[gpN]), genPartPts[gpN]);
-                    h_elEff_den->Fill(abs(genPartEtas[gpN]), genPartPts[gpN]);
-                }  
-                else if (genPartRecoStatus[gpN] == -11)
+                    h_tauEff_reco->Fill(genVisTauEtas[gvtN], genVisTauPts[gvtN]);
+                    h_tauEff_den->Fill(genVisTauEtas[gvtN], genVisTauPts[gvtN]);
+                }
+                else
                 {
-                    h_elEff_den->Fill(abs(genPartEtas[gpN]), genPartPts[gpN]);
-		}  
-                //Muons
-                else if (genPartRecoStatus[gpN] == 13)
-                {
-                    h_muEff_reco->Fill(abs(genPartEtas[gpN]), genPartPts[gpN]);
-                    h_muEff_den->Fill(abs(genPartEtas[gpN]), genPartPts[gpN]);
-                }  
-                else if (genPartRecoStatus[gpN] == -13)
-                {
-                    h_muEff_den->Fill(abs(genPartEtas[gpN]), genPartPts[gpN]);
-                }  
-                //Photons
-                else if (genPartRecoStatus[gpN] == 22)
-                {
-                    h_phoEff_reco->Fill(abs(genPartEtas[gpN]), genPartPts[gpN]);
-                    h_phoEff_den->Fill(abs(genPartEtas[gpN]), genPartPts[gpN]);
-                }  
-                else if (genPartRecoStatus[gpN] == -22)
-                {
-                    h_phoEff_den->Fill(abs(genPartEtas[gpN]), genPartPts[gpN]);
-                }  
+                    h_tauEff_den->Fill(genVisTauEtas[gvtN], genVisTauPts[gvtN]);
+                }
             }
+        }
     }
+
+    chain.SetBranchStatus("*", true); //Reset all branch statuses to true
 
     h_elEff_reco->Sumw2();
     h_elEff_den->Sumw2();
@@ -151,62 +198,62 @@ void incRecoEffs(TH2F* h_elEff, TH2F* h_muEff, TH2F* h_tauEff, TH2F* h_phoEff, T
     h_muEff_den->Sumw2();
     h_phoEff_reco->Sumw2();
     h_phoEff_den->Sumw2();
-    //h_tauEff_reco->Sumw2();
-    //h_tauEff_den->Sumw2();
+    h_tauEff_reco->Sumw2();
+    h_tauEff_den->Sumw2();
 
     //Now calculated the efficiencies
     h_elEff_reco->Divide(h_elEff_den);
     h_muEff_reco->Divide(h_muEff_den);
-    //h_tauEff_reco->Divide(h_tauEff_den);
+    h_tauEff_reco->Divide(h_tauEff_den);
     h_phoEff_reco->Divide(h_phoEff_den);
 
     if (h_elEff->GetEntries() == 0) //Passed histograms are empty, therefore we can just add our new efficiencies in
     {
         h_elEff->Add(h_elEff_reco);
         h_muEff->Add(h_muEff_reco);
-        //h_tauEff->Add(h_tauEff_reco);
+        h_tauEff->Add(h_tauEff_reco);
         h_phoEff->Add(h_phoEff_reco);
     }
     else //There are already efficiencies in the passed histograms, therefore total overall efficiency is multiplicative
     {
         h_elEff->Multiply(h_elEff_reco);
         h_muEff->Multiply(h_muEff_reco);
-        //h_tauEff->Multiply(h_tauEff_reco);
+        h_tauEff->Multiply(h_tauEff_reco);
         h_phoEff->Multiply(h_phoEff_reco);
     }
 
-    cout << "...Done calculating object reco efficiencies for " << year << endl;
+    std::cout << "...Done calculating object reco efficiencies for " << year << endl;
 }
 
 
 //Calculate the efficiencies chosen by the inc* parameters for each object in the analysis and save them to plots and a root file
 void calcObjEffs(TString outFilename, bool incReco=true, bool incID=true, bool incTrig=true)
 {
-    //const int N_YEARS = 4;
-    //TString years[N_YEARS] = {"2015", "2016", "2017", "2018"};
-    const int N_YEARS = 1;
-    TString years[N_YEARS] = {"2015"};
+    const int N_YEARS = 4;
+    TString years[N_YEARS] = {"2015", "2016", "2017", "2018"};
+    //const int N_YEARS = 1;
+    //TString years[N_YEARS] = {"2015"};
     
     //Set up canvases and graphics options
     gStyle->SetOptStat(0);
     gStyle->SetPaintTextFormat("4.2f");
     TCanvas* elCanv = new TCanvas("elCanv", "Electron Efficiencies", 1200, 800);
     TCanvas* muCanv = new TCanvas("muCanv", "Muon Efficiencies", 1200, 800);
-    //TCanvas* tauCanv = TCanvas("tauCanv", "Tauh Efficiencies", 1200, 800);
+    TCanvas* tauCanv = new TCanvas("tauCanv", "Tauh Efficiencies", 1200, 800);
     TCanvas* phoCanv = new TCanvas("phoCanv", "Photon Efficiencies", 1200, 800);
 
     if (N_YEARS >= 2 && N_YEARS % 2 == 0)
     {
         elCanv->Divide(2, N_YEARS/2);
         muCanv->Divide(2, N_YEARS/2);
-        //tauCanv->Divide(2, N_YEARS/2);
+        tauCanv->Divide(2, N_YEARS/2);
         phoCanv->Divide(2, N_YEARS/2);
     }
     else if (N_YEARS >= 2)
     {
         elCanv->Divide(2, N_YEARS/2 + 1);
         muCanv->Divide(2, N_YEARS/2 + 1);
-        //tauCanv->Divide(2, N_YEARS/2 + 1);
+        tauCanv->Divide(2, N_YEARS/2 + 1);
         phoCanv->Divide(2, N_YEARS/2 + 1);
     }
 
@@ -214,60 +261,73 @@ void calcObjEffs(TString outFilename, bool incReco=true, bool incID=true, bool i
     TFile* outfile = new TFile(outFilename, "RECREATE");
     TString outStr;
 
-    const int nEtaBins = 5;
-    double etaBins[nEtaBins + 1] = {0, 0.8, 1.4442, 1.566, 2.0, 2.5};
-    TString etaBinLabels[nEtaBins] = {"[0, 0.8)", "[0.8, 1.4442)", "[1.4442, 1.566)", "[1.566, 2.0)", "[2.0, 2.5)"};
+    double etaBins_EG[6] = {0, 0.8, 1.4442, 1.566, 2.0, 2.5};
+    TString etaBinLabels_EG[5] = {"[0, 0.8)", "[0.8, 1.4442)", "[1.4442, 1.566)", "[1.566, 2.0)", "[2.0, 2.5)"};
+    double etaBins_mu[5] = {0, 0.9, 1.2, 2.1, 2.4};
+    TString etaBinLabels_mu[4] = {"[0, 0.9)", "[0.9, 1.2)", "[1.2, 2.1)", "[2.1, 2.4)"};
+    double etaBins_tau[4] = {0, 1.46, 1.558, 2.3};
+    TString etaBinLabels_tau[3] = {"[0, 1.46)", "[1.46, 1.558)", "[1.558, 2.3)"};
+    double ptBins_mu[6] = {18, 50, 100, 200, 500, 1500};
+    double ptBins_EG[7] = {5, 50, 100, 200, 500, 1000, 4000};
+    double ptBins_tau[6] = {18, 50, 100, 200, 500, 1000};
 
 
     TString effsStr = buildEffsStr(incReco, incID, incTrig);    
 
     for (int yrN = 0; yrN < N_YEARS; yrN++)
     {
-        TH2F* h_elEff = new TH2F("h_elEff_" + years[yrN], years[yrN] + " Electron " + effsStr + " Efficiency;El |#eta|;El pt", nEtaBins, etaBins, 20, 0, 2000);
-        TH2F* h_phoEff = new TH2F("h_phoEff_" + years[yrN], years[yrN] +  " Photon " + effsStr + " Efficiency;Pho |#eta|;Pho pt", nEtaBins, etaBins, 20, 0, 2000);
-        TH2F* h_muEff = new TH2F("h_muEff_" + years[yrN], years[yrN] +  " Muon " + effsStr + " Efficiency;Mu |#eta|;Mu pt", nEtaBins, etaBins, 20, 0, 2000);
-        TH2F* h_tauEff = new TH2F("h_tauEff_" + years[yrN], years[yrN] +  " Tau_h " + effsStr + " Efficiency;Tau |#eta|;Tau pt", nEtaBins, etaBins, 20, 0, 2000);
+        TH2F* h_elEff = new TH2F("h_elEff_" + years[yrN], years[yrN] + " Electron " + effsStr + " Efficiency;El |#eta|;El pt", 5, etaBins_EG, 6, ptBins_EG);
+        TH2F* h_phoEff = new TH2F("h_phoEff_" + years[yrN], years[yrN] +  " Photon " + effsStr + " Efficiency;Pho |#eta|;Pho pt", 5, etaBins_EG, 6, ptBins_EG);
+        TH2F* h_muEff = new TH2F("h_muEff_" + years[yrN], years[yrN] +  " Muon " + effsStr + " Efficiency;Mu |#eta|;Mu pt", 4, etaBins_mu, 5, ptBins_mu);
+        TH2F* h_tauEff = new TH2F("h_tauEff_" + years[yrN], years[yrN] +  " Tau_h " + effsStr + " Efficiency;Tau |#eta|;Tau pt", 3, etaBins_tau, 5, ptBins_tau);
 	
-	h_elEff->Sumw2();
-	h_muEff->Sumw2();
-	h_phoEff->Sumw2();
-        //h_tauEff->Sumw2();
+        h_elEff->Sumw2();
+        h_muEff->Sumw2();
+        h_phoEff->Sumw2();
+        h_tauEff->Sumw2();
 
         if (incReco)
-            incRecoEffs( h_elEff, h_muEff, h_tauEff, h_phoEff, years[yrN], effsStr, nEtaBins, etaBins);   
+            incRecoEffs( h_elEff, h_muEff, h_tauEff, h_phoEff, years[yrN], effsStr);   
         
 	//Set better eta bin labels
-	for (int i = 0; i < nEtaBins; i++)
-	  {
-	    h_elEff->GetXaxis()->SetBinLabel(i+1, etaBinLabels[i]);
-	    h_muEff->GetXaxis()->SetBinLabel(i+1, etaBinLabels[i]);
-	    h_phoEff->GetXaxis()->SetBinLabel(i+1, etaBinLabels[i]);
-	    //h_tauEff->GetXaxis()->SetBinLabel(i+1, etaBinLabels[i]);
-	  }
+        for (int i = 0; i < 5; i++) //6 is the largest number of eta bins
+        {
+            h_elEff->GetXaxis()->SetBinLabel(i+1, etaBinLabels_EG[i]);
+            h_phoEff->GetXaxis()->SetBinLabel(i+1, etaBinLabels_EG[i]);
+            if (i < 4) //Only 4 mu bins
+            h_muEff->GetXaxis()->SetBinLabel(i+1, etaBinLabels_mu[i]);
+            if (i < 3) //only 3 tau eta bins
+            h_tauEff->GetXaxis()->SetBinLabel(i+1, etaBinLabels_tau[i]);
+        }
 
         //Now plot
         elCanv->cd(yrN+1);
         h_elEff->Draw("COLZ TEXTE");
+        gPad->SetLogy(1);
         gPad->Modified();
-	gPad->Update();
+        gPad->Update();
         muCanv->cd(yrN+1);
         h_muEff->Draw("COLZ TEXTE");
+        gPad->SetLogy(1);
         gPad->Modified();
         gPad->Update();
         phoCanv->cd(yrN+1);
         h_phoEff->Draw("COLZ TEXTE");
+        gPad->SetLogy(1);
         gPad->Modified();
         gPad->Update();
-        //tauCanv->cd(yrN);
-        //h_tauEff->Draw("COLZ TEXT");
-        //gPad->Modified();
-        //gPad->Update();
+        tauCanv->cd(yrN+1);
+        h_tauEff->Draw("COLZ TEXTE");
+        gPad->SetLogy(1);
+        gPad->Modified();
+        gPad->Update();
 
+        
         //Write the histograms to the output root file
         h_elEff->Write();
         h_muEff->Write();
         h_phoEff->Write();
-        //h_tauEff->Write();	    
+        h_tauEff->Write();	    
     }
 
     //Save the output plots
@@ -276,7 +336,7 @@ void calcObjEffs(TString outFilename, bool incReco=true, bool incID=true, bool i
     elCanv->SaveAs(outPath + "elEff_" + outStr);
     muCanv->SaveAs(outPath + "muEff_" + outStr);
     phoCanv->SaveAs(outPath + "phoEff_" + outStr);
-    //tauCanv->SaveAs(outPath + "tauEff_" + outStr);   
+    tauCanv->SaveAs(outPath + "tauEff_" + outStr);   
 
     outfile->Close();
 }
